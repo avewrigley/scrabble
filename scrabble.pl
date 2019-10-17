514f960cfae7d9e09addf3b3bef113c570a3fb88
Ave Wrigley <ave.wrigley@agileanalog.com>
2019-10-17 17:19:01 +0100

    Fixes to CLI

--- scrabble.pl
+++ scrabble.pl
@@ -4,14 +4,16 @@ use strict;
 use warnings;
 
 use lib 'lib';
+use FindBin qw( $Bin );
 use Scrabble;
 
 my $type = shift;
 my $word = shift;
 my $length = shift;
+my $word_file = "$Bin/words.txt";
 
 die "option must be one of (r|s|p|a)" unless defined $type and $type =~ /^(r|s|p|a)$/;
 
-my $scrabble = new Scrabble( length => $length, type => $type, word => $word );
+my $scrabble = new Scrabble( word_file => $word_file, length => $length, type => $type, word => $word );
 my @words = $scrabble->words();
-print map "$_\n", @words;
+print map "$_->{w} $_->{l} $_->{v}\n", @words;
