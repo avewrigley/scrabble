5546bc3b74df9dc30ffecee4326820d5c86a1652
Ave Wrigley <ave.wrigley@ave.wrigley.name>
2019-11-21 15:10:50 +0000

    use Getopt::Long for CLO

--- scrabble.pl
+++ scrabble.pl
@@ -7,13 +7,18 @@ use warnings;
 use lib 'lib';
 use FindBin qw( $Bin );
 use Scrabble;
+use Getopt::Long;
+use Text::Table;
 
-my $type = shift;
+my %opts = ( type => 'p' );
+my @opts = qw( type=s verbose );
+GetOptions( \%opts, @opts ) or pod2usage( verbose => 0 );
+
 my $word = shift;
-my $length = shift;
 my $word_file = "$Bin/words.txt";
 
-die "option must be one of (r|s|p|a)" unless defined $type and $type =~ /^(r|s|p|a)$/;
+die "option must be one of ([r]egex|[p]ermute|[a]nagram)" unless defined $opts{type} and $opts{type} =~ /^(r|p|a)/;
+
 
 my $scrabble = new Scrabble( word_file => $word_file, length => $length, type => $type, word => $word );
 my @words = $scrabble->words();
